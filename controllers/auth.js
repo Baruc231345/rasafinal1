c84480cf1dbca6f0137d1563a57ac1fb3479b3b6
Migol <baruc.231345@globalcity.sti.edu.ph>
2023-11-20 09:56:36 +0800

    1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
+
 const express = require("express");
-const csrfProtection = require("csurf");
 const register = require("./register");
 const login = require("./login");
 const editUserView = require("./editUserView");
@@ -8,36 +8,17 @@ const rasatesting2 = require("./rasatesting2");
 const rasatesting2_inventory = require("./rasatesting2_inventory");
 const rasatesting_inventory = require("./rasatesting_inventory");
 const insertSign = require("./insertSign");
-const calendarInput = require("./calendarInput");
-const rasa_view = require("./rasa_view");
+const calendarInput = require ("./calendarInput");
+const rasa_view = require ("./rasa_view");
 
 const router = express.Router();
-const csrfMiddleware = csrfProtection();
 router.post("/register", register)
 router.post("/rasa_view", rasa_view)
 router.post("/calendarInput", calendarInput)
 router.post("/editUserView", editUserView)
 router.post("/login", login)
 router.post("/rasatesting", rasatesting);
-router.post("/api/rasatesting2", csrfMiddleware, (req, res, next) => {
-    const csrfToken = req.body._csrf;
-  
-    if (usedCsrfTokens.has(csrfToken)) {
-      // If the token has already been used, reject the request
-      res.status(403).send("CSRF token already used");
-    } else {
-      // Proceed with the next middleware (or route handler)
-      usedCsrfTokens.add(csrfToken); // Mark the token as used
-      next();
-    }
-  });
-  
-  // Clear used CSRF tokens after a certain period or based on your requirements
-  setInterval(() => {
-    usedCsrfTokens.clear();
-  }, 24 * 60 * 60 * 1000); // Clear tokens every 24 hours, adjust as needed
-
-
+router.post("/rasatesting2", rasatesting2)
 router.post("/rasatesting2_inventory", rasatesting2_inventory)
 router.post("/rasatesting_inventory", rasatesting_inventory)
 router.post("/insertSign", insertSign)
