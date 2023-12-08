013c2d92a8242524fae803299d03e4c6fc7e910d
Migol <baruc.231345@globalcity.sti.edu.ph>
2023-12-08 13:09:49 +0800

    Update rasa-db.js

--- routes/rasa-db.js
+++ routes/rasa-db.js
@@ -4,7 +4,8 @@ const db1 = sql.createConnection({
     host: process.env.DATABASE_HOST1,
     user: process.env.DATABASE_USER1,
     password: process.env.DATABASE_PASSWORD1,
-    database: process.env.DATABASE1
+    database: process.env.DATABASE1,
+    connectTimeout: 20000,
 })
 /*
 const db1 = sql.createConnection({
